be78843a5a8f5b28b044ec263b1bc28875d86292
Alex F <majestic254@gmail.com>
2020-01-17 19:59:27 -0500

    Comment missing

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -98,7 +98,7 @@ else:
     # DATABASES = {
         # "default": {
             # "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         # }
     # }
 
